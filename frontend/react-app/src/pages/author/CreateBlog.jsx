d2f297db00db8bf35be0029c0ccea00c842c3e35
anshul-303 <patilanshul27@gmail.com>
2026-02-28 15:50:32 +0530

    Done with skeleton of create blog page.

--- frontend/react-app/src/pages/author/CreateBlog.jsx
+++ frontend/react-app/src/pages/author/CreateBlog.jsx
@@ -1,8 +1,6 @@
-import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar.jsx";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { LogoutUser } from "../../api/authApi/authApi.js";
 import { useAuth } from "../../contexts/authContext.jsx";
 import { checkAuth } from "../../api/authApi/authApi.js";
 import { useRole } from "../../contexts/roleContexts.jsx";
@@ -51,20 +49,29 @@ export default function CreateBlog() {
 
   return (
     <>
-      <div className="overflow-hidden">
+      <div className="text-white bg-zinc-800 overflow-hidden">
         <Navbar />
         <div className="max-w-screen md:h-[4vw] h-[4vh] flex justify-center items-center text-white text-[1.5em] bg-zinc-900 border-b-[1px] border-zinc-600">
           <p className=" w-[97.5%] flex justify-center items-center">
-            Create a Blog{" "}
+            Create a Blog
           </p>
         </div>
-        <div className="w-full flex flex-col md:flex-row   ">
-          <div className="w-full min-h-[50vh] md:w-[60vw] md:min-h-[100vh] bg-zinc-800 flex justify-center items-center border-r-[2px] border-zinc-700">
-            Input div
+        <div className="w-full flex flex-col py-4 ">
+          <div className="p-1  max-w-full min-h-[50vh] bg-zinc-800 flex justify-center items-center border-white border-zinc-700">
+            <div className="w-[97.5%] min-h-[50vh] flex justify-center items-center border border-white text-white rounded-sm flex-col ">
+              Input div
+            </div>
           </div>
-          <div className="w-full min-h-[50vh] md:w-[40vw] md:min-h-[100vh]  bg-zinc-800 flex justify-center items-center">
-            Preview div
+          <div className="p-1 max-w-full min-h-[50vh] bg-zinc-800 flex justify-center items-center  border-t-[2px] border-white border-zinc-700">
+            <div className="w-[97.5%] min-h-[50vh] flex justify-center items-center border border-white text-white rounded-sm">
+              Preview div
+            </div>
           </div>
+        </div>
+        <div className="max-w-screen md:h-[4vw] h-[4vh] flex justify-center items-center text-white text-[1.5em] bg-zinc-900 border-b-[1px] border-zinc-600">
+          <p className=" w-[97.5%] flex justify-center items-center">
+            The submit and draft options
+          </p>
         </div>
       </div>
     </>

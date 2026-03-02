b8705ae74248b956d93099793b53248ed3831ae6
anshul-303 <patilanshul27@gmail.com>
2026-03-02 09:35:09 +0530

    Small change in heading

--- frontend/react-app/src/pages/author/CreateBlog.jsx
+++ frontend/react-app/src/pages/author/CreateBlog.jsx
@@ -64,7 +64,7 @@ export default function CreateBlog() {
         </div>
         <div className="w-full flex flex-col py-4 ">
           <div className="p-1  max-w-full min-h-[50vh] bg-zinc-800 flex justify-center items-center border-white border-zinc-700">
-            <div className="w-[97.5%] min-h-[50vh] flex flex-col py-3 px-7 text-[1.25em] border border-white text-white rounded-sm gap-2 pb-5 ">
+            <div className="w-[97.5%] min-h-[50vh] flex flex-col py-3 px-7 text-[1.25em]  border border-zinc-700 border-[2px] text-white rounded-sm gap-2 pb-5 ">
               <p className="w-[97.5%] text-[1.4em] font-bold uppercase tracking-widest text-zinc-200">
                 Blog details
               </p>
@@ -134,31 +134,6 @@ export default function CreateBlog() {
                       hover:border-zinc-700 transition-all duration-200"
                 />
               </div>
-              {/* <div className="w-[97.5%] flex flex-col md:flex-row  border-white rounded-sm  justify-center items-center">
-                <div className=" w-[95%] h-[50vh] sm:w-[70%] sm:h-[60vh] flex flex-col justify-top items-center rounded-sm gap-5 sm:py-4">
-                  <label className="text-xl font-bold uppercase tracking-widest text-zinc-500 ml-1">
-                    Head image
-                  </label>
-                  <textarea
-                    value={headimageUrl}
-                    onChange={(e) => {
-                      setHeadImageUrl(e.target.value);
-                    }}
-                    className="w-full h-[70%] sm:w-[95%] sm:min-h-[45vh] p-2 rounded-sm
-                    bg-zinc-900 text-white placeholder-zinc-600
-                    border border-zinc-800
-                    focus:outline-none focus:border-white/40 focus:ring-1 focus:ring-white/10
-                    hover:border-zinc-700 transition-all duration-200 "
-                    placeholder="Enter the head image URL..."
-                  />
-                </div>
-                <div className="flex justify-center items-center sm:w-[35%] sm:h-[55vh] border border-zinc-700 p-1 rounded-sm border-[3px] ">
-                  <img
-                    src="https://images.pexels.com/photos/5867948/pexels-photo-5867948.jpeg"
-                    className="w-[98%] h-[98%] rounded-sm "
-                  />
-                </div>
-              </div> */}
 
               <div className="w-[97.5%] flex flex-col lg:flex-row gap-6 p-4 lg:p-1">
                 {/* INPUT CARD */}
@@ -190,10 +165,7 @@ export default function CreateBlog() {
 
                   <div className="relative w-full aspect-[4/3] rounded-md overflow-hidden border border-zinc-700 bg-zinc-950 flex items-center justify-center">
                     <img
-                      src={
-                        headimageUrl ||
-                        "/image_placeholder.jpg"
-                      }
+                      src={headimageUrl || "/image_placeholder.jpg"}
                       alt="Head preview"
                       className="w-full h-full object-cover transition-transform duration-300 hover:scale-[1.02]"
                     />
@@ -202,13 +174,15 @@ export default function CreateBlog() {
               </div>
             </div>
           </div>
-
-          <div className="p-1 max-w-full min-h-[50vh] bg-zinc-800 flex justify-center items-center  border-t-[2px] border-white border-zinc-700">
+          <p className="p-2 w-full flex justify-center items-center text-zinc-300 text-[2rem] font-bold text-md my-2">
+            BLOG PREVIEW
+          </p>
+          <div className="p-1 max-w-full min-h-[50vh] bg-zinc-800 flex justify-center items-center border-white border-zinc-700">
             <div
-              className="w-[97.5%] min-h-[50vh] flex justify-center items-center border border-white text-white rounded-sm"
+              className="w-[97.5%] min-h-[50vh] flex justify-center items-center border border-zinc-700 border-[2px] text-white rounded-sm"
               alt="This is an image"
             >
-              Preview div
+              
             </div>
           </div>
         </div>

0ae163f71aced2daf6d99b133f88ad3a3f8486d7
anshul-303 <patilanshul27@gmail.com>
2026-03-29 22:42:35 +0530

    general

--- frontend/react-app/src/pages/author/Dashboard.jsx
+++ frontend/react-app/src/pages/author/Dashboard.jsx
@@ -1,12 +1,61 @@
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar.jsx";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/authContext.jsx";
+import { checkAuth } from "../../api/authApi/authApi.js";
+import { useRole } from "../../contexts/roleContexts.jsx";
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+  const { isAuthenticated, setIsAuthenticated } = useAuth();
+  const { role, setRole } = useRole();
+  const url = import.meta.env.VITE_APP_API_URL;
+
+  useEffect(() => {
+    const callCheckAuthAPI = async () => {
+      await checkAuth(navigate, setIsAuthenticated, setRole);
+    };
+    if (!isAuthenticated) {
+      callCheckAuthAPI();
+    }
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    if (!isAuthenticated) return;
+
+    // Guard: Role-based access
+    if (role !== "author" && role) {
+      navigate("/403");
+      return;
+    }
+    const fetchtrial = async () => {
+      const res = await fetch(`${url}/test`, {
+        credentials: "include",
+      });
+      if (res.status === 403 || res.status === 401) {
+        const newres = await fetch(`${url}/auth/refresh`, {
+          credentials: "include",
+        });
+        if (newres.ok) {
+          fetchtrial();
+        } else {
+          throw Error("The access token is not there.");
+        }
+      }
+    };
+
+    fetchtrial();
+  }, [isAuthenticated, role, navigate]);
+
   return (
-    <div className="justify-center flex flex-col items-center h-screen w-screen text-center text-[2em] bg-zinc-900 text-white">
-      <p>Dashboard Page</p>
-      <p className="underline">
-        <Link to={"/menu"}>Menu</Link>{" "}
-      </p>
+    <div className="text-white bg-zinc-800 overflow-hidden">
+      <Navbar />
+      <div className="w-screen md:h-[10vh] h-[7vh] bg-zinc-900 flex justify-between items-center pl-5 pr-5 md:pl-5 md:pr-10 ">
+        <p className="text-2xl font-bold text-white">Author Dashboard</p>
+        <button className="text-zinc-900 font-semibold bg-white border rounded-lg border-[2px] py-2 px-4 md:px-8 hover:bg-zinc-100 active:bg-zinc-300">New Article</button>
+      </div>
+      <div className="w-screen h-[100vh] bg-zinc-800"></div>
     </div>
   );
 }
